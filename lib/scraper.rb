ce0b9c3d8d446f994d2c7e160eec218ae158e4aa
Rafael Batista <rafaelb24@yahoo.com>
2018-11-19 23:51:30 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,7 +21,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
     links = doc.css(".social-icon-container").css("a")
-    quote = doc.css(".vital-text-container").css(".profile_quote")
+    quote = doc.css(".vital-text-container").css(".profile_quote").text
     info = {}
     links.each do |link|
       a = link.attributes["href"].value
