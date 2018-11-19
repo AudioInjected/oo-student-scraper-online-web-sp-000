bfcf452f1dabd3f0fd678587bee1cd8698be3d94
Rafael Batista <rafaelb24@yahoo.com>
2018-11-19 22:39:39 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,6 +12,7 @@ class Scraper
       info[:name] = student.css("h4.student-name").text
       info[:location] = student.css("p.student-location").text
       info[:profile_url] = student.xpath("//div/a/@href").text
+      binding.pry
       array << info
     end
     array
