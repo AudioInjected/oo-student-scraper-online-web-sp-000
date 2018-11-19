1dbca140d214002c718ec50c659ab116394903e3
Rafael Batista <rafaelb24@yahoo.com>
2018-11-19 22:35:55 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,22 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    students = doc.css(".student-card")
+    array = []
+    students.each do |student|
+      info = {}
+      info[:name] = student.css("h4.student-name").text
+      info[:location] = student.css("p.student-location")
+      info[:profile_url] = student.xpath("//div/a/@href")
+      array << info
+    end
+    array
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
